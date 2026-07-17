bcd5a984d199a97751c30aea6e69512d7d44a4fe
routersys <github.routersys@gmail.com>
2026-07-18 07:51:29 +0900

    無駄な拡張を元に戻す

--- Fog/FogEffect.cs
+++ Fog/FogEffect.cs
@@ -27,8 +27,8 @@ namespace Fog
         public Animation Unevenness { get; } = new Animation(50, 0, 100);
 
         [Display(GroupName = nameof(Texts.FogEffectName), Name = nameof(Texts.FogGradient), Description = nameof(Texts.FogGradientDescription), Order = 3, ResourceType = typeof(Texts))]
-        [AnimationSlider("F1", "%", -100, 100)]
-        public Animation Gradient { get; } = new Animation(0, YMM4Constants.VerySmallValue, YMM4Constants.VeryLargeValue);
+        [AnimationSlider("F2", "%", -100, 100)]
+        public Animation Gradient { get; } = new Animation(0, -100, 100);
 
         [Display(GroupName = nameof(Texts.FogEffectName), Name = nameof(Texts.FogFlowSpeed), Description = nameof(Texts.FogFlowSpeedDescription), Order = 4, ResourceType = typeof(Texts))]
         [AnimationSlider("F1", "%", -200, 200)]
